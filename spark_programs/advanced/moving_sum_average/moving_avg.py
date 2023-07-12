67bad0f63a23007f3162919282d444df2d9188d8
SanjayBedwal <sanjay.bedwal@gmail.com>
2023-07-12 11:53:41 +0530

    adding some documentation

--- spark_programs/advanced/moving_sum_average/moving_avg.py
+++ spark_programs/advanced/moving_sum_average/moving_avg.py
@@ -2,7 +2,7 @@
 Consider a scenario where you have a DataFrame containing stock market data for different stocks, and you want to calculate 
 the 3-days moving average for each stock based on their closing prices. 
 Additionally, you want to identify stocks that have experienced a significant increase or decrease in price compared to their 
-moving average.
+moving average.Conisder a change of +5 as a significant increase and -5 as a significant decrease in price
 '''
 
 from pyspark.sql import SparkSession
